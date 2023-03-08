3ffa32155132f50ffba45daa8eadc0e2838caac7
Elhameed <a.ajani@alustudent.com>
2023-03-08 18:41:11 +0200

    added new advanced questions

--- advanced_questions.py
+++ advanced_questions.py
@@ -54,4 +54,72 @@ PROBLEM_SETS = [
         },
         "answer": "b"
     }
+    {
+        "question": "What is the difference between the print() function and the return statement in Python?",
+        "options": {
+            "a": "print() displays output to the console, while return statement returns a value from a function",
+            "b": "print() is used for debugging purposes, while return statement is used to terminate a loop",
+            "c": "print() and return statement are interchangeable and can be used interchangeably",
+            "d": "print() and return statement are not valid functions in Python"
+        },
+        "answer": "a"
+    }
+    {
+        "question": "What is the purpose of the urllib module in Python?",
+        "options": {
+            "a": "To perform HTTP requests and handle responses",
+            "b": "To generate random numbers",
+            "c": "To manipulate strings",
+            "d": "To perform mathematical operations"
+        },
+        "answer": "a"
+    }
+    {
+        "question": "What is the purpose of test-driven development (TDD) in software development?",
+        "options": {
+            "a": "To ensure that code is thoroughly tested before it is released to production",
+            "b": "To write tests after the code has been written to ensure it works properly",
+            "c": "To write code without testing and fix any bugs that arise later",
+            "d": "To write code quickly without worrying about testing"
+        },
+        "answer": "a"
+    }
+    {
+        "question": "What is object-relational mapping (ORM) in Python?",
+        "options": {
+            "a": "A technique for mapping objects in Python to relational database tables",
+            "b": "A technique for mapping Python functions to RESTful APIs",
+            "c": "A technique for mapping Python modules to object-oriented programming (OOP) concepts",
+            "d": "A technique for mapping Python scripts to network protocols"
+        },
+        "answer": "a"
+    }
+    {
+        "question": "What is the difference between the input() function and the raw_input() function in Python 2.x?",
+        "options": {
+            "a": "The input() function returns a string, while the raw_input() function returns the input as it is typed",
+            "b": "The raw_input() function is not a valid function in Python 2.x",
+            "c": "The input() function is not a valid function in Python 2.x",
+            "d": "The input() and raw_input() functions are interchangeable and can be used interchangeably in Python 2.x"
+        },
+        "answer": "a"
+    }
+    {
+        "question": "Which of the following is a valid way to open a file named \"example.txt\" in read mode?",
+        "options": {
+            "a": "file = open(\"example.txt\", mode=\"w\")",
+            "b": "file = open(\"example.txt\", mode=\"r+\")"
+        },
+        "answer": "b"
+    },
+    {
+        "question": "What is the output of the following code?\n\ndef greet(name):\n    return f\"Hello, {name}!\"\n\nprint(greet(\"Alice\"))",
+        "options": {
+            "a": "Hello, Alice!",
+            "b": "Hello, !",
+            "c": "TypeError: greet() missing 1 required positional argument: 'name'",
+            "d": "SyntaxError: invalid syntax"
+        },
+        "answer": "a"
+    },
 ]
